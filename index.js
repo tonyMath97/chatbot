39a892f2a2cc289a72b39711a9483d37bee56d25
unknown <antony.matheus6849@gmail.com>
2020-07-15 17:31:34 -0300

    adicionando bodyParser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express( );
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) =>{
     res.send("Hello WORD");
