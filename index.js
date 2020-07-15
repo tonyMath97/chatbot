c9b9f8227aab9fb45a3613ab9f375b270b0d99a7
unknown <antony.matheus6849@gmail.com>
2020-07-15 17:24:47 -0300

    aprendendo http

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express( );
+
+
+
+app.get('/', (req, res) =>{
+    res.send("Hello WORD");
+})
+
+// escrevendo textos no host
+app.get('/pergunta', (req, res) => {
+  //console.log(req.query);
+  // via query
+  msg=req.query.pergunta;
+  res.send('Você perguntou :' +msg);
+})
+//implementando a varialvel que eu quero
+// duas variais implementadas na edição
+// via params
+app.get('/mensagem/:tipo/:id', (req,res)=>{
+   msg=req.params.tipo;
+   cod=res.params.id;
+   res.send("resposta que você editar o id : " + cod);
+})
+
+app.post('/mensagem', (req,res) =>{
+  console.log(req.body);
+  res.send("msg recebida");
+})
+
+app.listen( 3000, "127.0.0.1", () =>{
+    console.log("Servidor rodando");
+
+})
